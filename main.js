da19413067d34b7f7cf5ca7bfea9bd1c207b2b0d
Pabllo Dantas <pabllo@ascentprojetos.com.br>
2024-03-25 11:52:13 -0300

    Implamentação de funcionalidades para a lista de animais e faq.

---
+++ main.js
@@ -0,0 +1,43 @@
+function initTavNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
+
+  if (tabMenu.length && tabConteudo.length) {
+    tabConteudo[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabConteudo.forEach((item) => {
+        item.classList.remove("ativo");
+      });
+      tabConteudo[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initTavNav();
+initAccordion();
