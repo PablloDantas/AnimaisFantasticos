8a0bc929670029e982c7df8963e872132c010150
Pabllo Dantas <pabllo@ascentprojetos.com.br>
2024-03-25 12:11:55 -0300

    feat: adição das animações de scroll nas sections principais.

--- main.js
+++ main.js
@@ -39,5 +39,25 @@ function initAccordion() {
   }
 }
 
+function initAnimaScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+
+  window, addEventListener("scroll", animaScroll);
+
+  animaScroll();
+}
+
 initTavNav();
 initAccordion();
+initAnimaScroll();
