6a62d7d1eec685f1c010c029b7570201144ff344
Pabllo Dantas <pabllo@ascentprojetos.com.br>
2024-04-23 07:30:41 -0300

    feat: adição do modal para login.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initAnimaScroll from "./modules/anima-scroll.js";
 import initTavNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
+import initModal from "./modules/modal.js";
 
 initAnimaScroll();
 initTavNav();
 initAccordion();
+initModal();
